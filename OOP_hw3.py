65421ec7090cc9a3df0491dd7733cd78df636c69
Angelina Koroleva <angvkoroleva@gmail.com>
2025-06-21 12:41:39 +0300

    Forth task

--- OOP_hw3.py
+++ OOP_hw3.py
@@ -1,11 +1,4 @@
 class Student:
-    def rate_hw(self, student, course, grade):
-        pass
-
-    def __len__(self):
-        return len(self.finished_courses)
-    
-
     def __init__(self, name, surname, gender):
         self.name = name
         self.surname = surname
@@ -31,14 +24,41 @@ class Student:
                 count += 1
         lecturer.average = sum / count
     
+    def get_average_by_course(self, course): #average grade of 1 student for current course
+        for grade in self.grades.items():
+            if (grade[0] in course):
+                count = 0
+                average_mark_for_current_grade = 0
+                for mark in grade[1]:
+                    average_mark_for_current_grade += mark
+                    count +=1
+        # print(average_mark_for_current_grade / count)
+        return average_mark_for_current_grade / count
+
+
+    
+    def __len__(self):
+        return len(self.finished_courses)
+    
+
     def __lt__(self, other):
         return self.average < other.average
        
     def __str__(self):
-
         return f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {self.average}\n\
 Курсы в процессе изучения: {(', ').join(self.courses_in_progress)}\nЗавершенные курсы: {(', ').join(self.finished_courses)}"
 
+
+def average_grade(students_list, course):
+    summa = 0
+    count = 0
+    for student in students_list:
+        if(course in student.courses_in_progress):
+            # print(student.get_average(course))
+            summa += student.get_average_by_course(course)
+            count += 1
+            #print(count, summa)
+    return (summa/count)
 
 class Mentor:
     def __init__(self, name, surname):
@@ -53,6 +73,18 @@ class Lecturer(Mentor):
         self.courses_attached = []
         self.grades = {}
         self.average = 0
+
+    def get_average_by_course(self, course): #average grade of 1 lecturer for current course
+        for grade in self.grades.items():
+            if (grade[0] in course):
+                count = 0
+                average_mark_for_current_grade = 0
+                for mark in grade[1]:
+                    average_mark_for_current_grade += mark
+                    count +=1
+        # print(average_mark_for_current_grade / count)
+        return average_mark_for_current_grade / count
+
     def __lt__(self, other):
         return self.average < other.average
     def __str__(self):
@@ -79,6 +111,16 @@ class Reviewer(Mentor):
         
     def __str__(self):
         return f"Имя: {self.name}\nФамилия: {self.surname}"    
+
+def average_grade_lecturer(lecturer_list, course):
+    summa = 0
+    count = 0
+    for lecturer in lecturer_list:
+        if(course in lecturer.courses_attached):
+            summa += lecturer.get_average_by_course(course)
+            count += 1
+            #print(count, summa)
+    return (summa/count)
 
 
 lecturer = Lecturer('Иван', 'Иванов')
@@ -109,19 +151,25 @@ reviewer.rate_hw(student1, 'C++', 8)
 reviewer.rate_hw(student1, 'C++', 10)
 reviewer.rate_hw(student1, 'Python', 2)
 
-if(student < student1):
-    print('1 хуже 2')  
-else:
-    print('1 лучше 2')  
+# if(student < student1):
+#     print('1 хуже 2')  
+# else:
+#     print('1 лучше 2')  
 
-if(lecturer < lecturer1):
-    print('1 хуже 2')  
-else:
-    print('1 лучше 2')  
+# if(lecturer < lecturer1):
+#     print('1 хуже 2')  
+# else:
+#     print('1 лучше 2')  
 
 
-print(reviewer)
-print(lecturer)
-print(lecturer1)
+# print(reviewer)
+# print(lecturer)
+# print(lecturer1)
 # print(student)
+student_list = [student, student1]
+print(average_grade(student_list, 'C++'))
+
+lecturer_list = [lecturer, lecturer1]
+print(average_grade_lecturer(lecturer_list, 'C++'))
+
 
